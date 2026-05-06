122e7c1999b393d8926d27a32e552d9963f32a47
Elitemanu <nnokammanuchim@gmail.com>
2026-05-06 16:22:21 +0100

    Hello program

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello{
+	public static void main(String[] args) {
+		System.out.println("Hello everyone, I am learning java"); 
+		System.out.print("oh wow, learning Java is fun"); 
+		System.out.print("Hurray, I am happy");
+ 		System.out.printf("Java was created in %d",1992);
+		System.out.printf("Java was made free for %c%.2f",'$',0.0000); 
+		System.out.printf("Do you love Java %c %b",'?', true);
+		System.out.printf("%s","Yes learning %s is fun","Java");
+	} 
+ }
+ 
