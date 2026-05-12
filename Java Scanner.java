7a88b0052bc5993043a9821f47cc903acaf4bbcd
Elitemanu <nnokammanuchim@gmail.com>
2026-05-12 09:22:35 +0100

    Main Chapter 1

---
+++ Java Scanner.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class UserInput{
+	public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter your name:");
+		String name = inpput.nextline();
+		
+		System.out.printf("Hello %s, you are welcome to NIIT",name);
+		
+	}
+	
+} 	
